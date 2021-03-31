eb5938c2887f8326b004300d09d9b2f43a9fb540
Luís Dourado <52614953+luisdourado33@users.noreply.github.com>
2021-03-31 19:37:22 -0300

    bugfix: Alterações em Login

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -3,14 +3,43 @@ import {
   BrowserRouter as Router,
   Route,
   RouteComponentProps,
+  Link,
 } from 'react-router-dom';
 
 import Login from './screens/Login';
 
+const divTemp = {
+  backgroundColor: 'orange',
+  padding: '30px',
+};
+const Home: React.FC = () => {
+  return (
+    <div style={divTemp}>
+      <h1>
+        <b>Job Finder:</b> Cronograma
+      </h1>
+      <ul>
+        <li>
+          <Link to='/login'>Página de Login (87%)</Link>
+        </li>
+        <li>
+          <Link to='/cadastro'>Página de cadastro (0%)</Link>
+        </li>
+        <li>
+          <Link to='/dashboard'>Página de dashboard (0%)</Link>
+        </li>
+        <li>
+          <Link to='/vagas'>Página de vagas (0%)</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 const AppRouter: React.FC = () => {
   return (
     <Router>
-      <Route path='/login' exact component={Login} />
+      <Route path='/' exact component={Home} />
+      <Route path='/login' component={Login} />
     </Router>
   );
 };
