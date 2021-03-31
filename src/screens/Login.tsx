9945c81cad979645ae528b786e422615b1dfa91d
Luís Dourado <52614953+luisdourado33@users.noreply.github.com>
2021-03-31 16:00:29 -0300

    ft: Wrapper na tela de login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,54 +1,53 @@
 import React from 'react';
-import { Container } from '@chakra-ui/react';
+// import { Container } from '@chakra-ui/react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  background-color: #161d6f;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: auto;
+`;
+
 const Wrapper = styled.div`
-  background-color: #98ded9;
-  color: white;
-  padding: 5px;
+  background-color: #fff;
+  margin: 30px;
   flex-direction: row;
   display: flex;
   flex: 1;
 `;
 
-const ImageHeader = styled.div`
-  background-image: url('https://images.unsplash.com/photo-1553877522-43269d4ea984?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80');
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position: center;
-  width: 100px;
+const ImageSide = styled.div`
+  background-color: orange;
   flex: 1;
-  display: flex;
-`;
-
-const FormHeader = styled.div`
-  background-color: #161d6f;
   padding: 30px;
-  display: flex;
-  flex: 1;
 `;
 
-const Title = styled.h1`
-  color: #333;
+const FormSide = styled.div`
+  background-color: orangered;
+  flex: 1;
+  padding: 30px;
 `;
 
 const Login: React.FC = () => {
   return (
     <Container>
       <Wrapper>
-        <ImageHeader>
-          <h1>Hello WORLD</h1>
-        </ImageHeader>
-        <FormHeader>
-          <h1>Hello World</h1>
+        <ImageSide>
+          <h1>Image Side</h1>
+        </ImageSide>
+        <FormSide>
+          <h1>Form Side</h1>
           <p>
-            Excepteur dolore ex et mollit eiusmod aute ipsum ex cupidatat elit
-            consectetur. Enim ea id deserunt et. Duis voluptate ullamco sunt
-            irure culpa ut sunt commodo proident id commodo occaecat.
-            Exercitation officia labore cillum velit sint ex tempor nostrud nisi
-            aliqua qui elit aute incididunt. Amet ullamco id veniam nulla.
+            Voluptate proident eu commodo sint incididunt tempor ipsum aliquip
+            cillum sint. Ullamco reprehenderit consectetur et commodo pariatur
+            reprehenderit incididunt quis voluptate anim. Ipsum amet enim veniam
+            proident dolor aliquip ex.
           </p>
-        </FormHeader>
+        </FormSide>
       </Wrapper>
     </Container>
   );
